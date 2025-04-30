6c29190c64543146f55378ea152ec279fdf9e5d5
KARAN NANA KATAKDHOND <karankatakdhond23@gmail.com>
2025-04-30 12:06:55 +0530

    Deleted Unnecesary files

--- map.dart
+++
@@ -1,8 +0,0 @@
-void main(){
-  List fruits = ['Mango','Lemon','Apple'];
-  var newfruits =  fruits.map((fruits){
-    return fruits.toUpperCase();
-  }).toList();
-  //fruits.toUpperCase();//throws error
-  print(newfruits);
-}
